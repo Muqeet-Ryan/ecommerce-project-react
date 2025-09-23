af1f8778332df4abcad5759070b822eec702a2f1
Muqeet-Ryan <muqeetryan@outlook.com>
2025-09-23 18:23:46 +0600

    Added Styles & Added header component & created HomePage

--- ecommerce-project-react/src/App.jsx
+++ ecommerce-project-react/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { Routes,Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
 
 function App() {
 
   return (
     <>
-
+    <Routes>
+      <Route index   element={<HomePage />}  />
+    </Routes>
+      
     </>
   )
 }
